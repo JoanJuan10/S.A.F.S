058b91c3496e2e40f870a9abde2b4be962d7f0df
JoanJuan10 <37046841+JoanJuan10@users.noreply.github.com>
2019-01-10 21:37:11 +0100

    Rework Completo del Juego
    Esto es una gran actualización que necesita mucho testeo por mi parte. Espero no que todo esté bien y poder convertir esto a "master" en cuanto me sea posible

--- Juego.java
+++ Juego.java
@@ -6,114 +6,98 @@ import java.util.Scanner;
 
 public class Joc {
 	BufferedReader bufEntrada = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void daño (int cantidadDaño, int mana, int jugadorAfectado) {
 
-	public static int firewave() {
-		int damage;
-		// Ataque de Jugador
-		damage = (int)(Math.random() * ((450 - 150) + 1)) + 150;
-		return damage;
+		if (jugadorAfectado == 1) {
+			if (escudoJugador) {
+				turnosEscudoJugador--;
+				System.out.println("Has bloqueado el ataque enemigo!");
+				if (turnosEscudoJugador == 0) {
+					escudoJugador = false;
+					System.out.println("El escudo enemigo se ha roto!");
+				}
+			}
+			else {
+				System.out.println("El enemigo ha perdido " + mana + " puntos de mana");
+				mp2 -= mana;
+				System.out.println("Has recibido " + cantidadDaño + " puntos de daño");
+				hp1 -= cantidadDaño;
+			}
+		}
+		else {
+			if (escudoMaquina) {
+				turnosEscudoMaquina--;
+				System.out.println("El enemigo ha bloqueado tu ataque!");
+				if (turnosEscudoMaquina == 0) {
+					escudoMaquina = false;
+					System.out.println("El escudo enemigo se ha roto!");
+				}
+			}
+			else {
+				System.out.println("Has perdido " + mana + " puntos de mana");
+				mp1 -= mana;
+				System.out.println("Has realizado " + cantidadDaño + " puntos de daño al enemigo");
+				hp2 -= cantidadDaño;
+			}
+		}
+	}
+	public static void curacion (int cantidadCurado, int mana, int jugadorAfectado) {
+
+		if (jugadorAfectado == 1) {
+			System.out.println("Has perdido " + mana + " puntos de mana");
+			mp1 -= mana;
+			System.out.println("Te has curado " + cantidadCurado + " puntos de vida");
+			hp1 += cantidadCurado;
+		}
+		else {
+			System.out.println("El enemigo ha perdido " + mana + " puntos de mana");
+			mp2 -= mana;
+			System.out.println("El enemigo se ha recuperado " + cantidadCurado + " puntos de vida");
+			hp2 += cantidadCurado;
+		}
+	}
+	public static void escudo (int cantidadTurnos, int mana, int jugadorAfectado) {
+		if (jugadorAfectado == 1) {
+			System.out.println("Has perdido " + mana + " puntos de mana");
+			mp1 -= mana;
+			System.out.println("Has obtenido un escudo! (DURACION: " + cantidadTurnos + " TURNOS)");
+			escudoJugador = true;
+			turnosEscudoJugador = cantidadTurnos;
+		}
+		else {
+			System.out.println("El enemigo ha perdido " + mana + " puntos de mana");
+			mp2 -= mana;
+			System.out.println("El enemigo obtiene un escudo! (DURACION: " + cantidadTurnos + " TURNOS)");
+			escudoMaquina = true;
+			turnosEscudoMaquina = cantidadTurnos;
+		}
 	}
 
-	public static int bioattack() {
-		int damage;
-		// Ataque de Jugador
-		damage = (int)(Math.random() * ((650 - 250) + 1)) + 250; //aleatorio(250,650);
-		return damage;
-	}
-
-	public static int heal() {
-		int curar;
-		// Ataque de Jugador
-		curar = (int)(Math.random() * ((600 - 500) + 1)) + 500;//aleatorio(500,600);
-		return curar;
-	}
-
-	public static int ataque() {
-		int damage;
-		// Ataque de Jugador
-		damage = (int)(Math.random() * ((150 - 100) + 1)) + 100;//aleatorio(100,150);
-		return damage;
-	}
-
-	public static int destruction() {
-		int damage;
-		// Ataque de Jugador ESPECIAL
-		damage = (int)(Math.random() * ((900 - 100) + 1)) + 100;//aleatorio(100,900);
-		return damage;
-	}
-
-	// ATAQUES DE MAQUINA
-	public static int ataque2() {
-		int damage;
-		// Ataque de Maquina
-		damage = (int)(Math.random() * ((200 - 150) + 1)) + 150;//aleatorio(150,200);
-		return damage;
-	}
-
-	public static int heal2() {
-		int curar;
-		// Ataque de Maquina
-		curar = (int)(Math.random() * ((550) + 1)) + 550;//aleatorio(550,650);
-		return curar;
-	}
-
-	public static int sdash() {
-		int damage;
-		// Ataque de Maquina
-		damage = (int)(Math.random() * ((500 - 250) + 1)) + 250;//aleatorio(250,500);
-		return damage;
-	}
-
-	public static int shadoworb() {
-		int damage;
-		// Ataque de Maquina
-		damage = (int)(Math.random() * ((400) + 1)) + 400;//aleatorio(400,700);
-		return damage;
-	}
-
-	public static int blackhole() {
-		int damage;
-		// Ataque de Maquina
-		damage = (int)(Math.random() * ((1400 - 500) + 1)) + 500;//aleatorio(500,1400);
-		return damage;
-	}
-
-	public static int arain() {
-		int damage;
-		// Ataque de Maquina
-		damage = (int)(Math.random() * ((400 - 100) + 1)) + 100;//aleatorio(100,400);
-		damage += (int)(Math.random() * ((100 - 50) + 1)) + 50;//damage+aleatorio(50,100);
-		damage += (int)(Math.random() * ((80 - 40) + 1)) + 40;//damage+aleatorio(40,80);
-		damage += (int)(Math.random() * ((80 - 30) + 1)) + 30;//damage+aleatorio(30,80);
-		damage += (int)(Math.random() * ((70 - 15) + 1)) + 15;//damage+aleatorio(15,70);
-		return damage;
-	}
-
+	public static int hp1 = 0;
+	public static int hp2 = 0;
+	public static int mp1 = 0;
+	public static int mp2 = 0;
+	public static boolean escudoJugador = false;
+	public static boolean escudoMaquina = false;
+	public static int turnosEscudoJugador = 0;
+	public static int turnosEscudoMaquina = 0;
+	
+	/*
+	 * Para obtener un numero random:
+	 * (int)(Math.random() * ((MAX - MIN) + 1)) + MIN
+	 */
 	public static void juego() throws IOException, InterruptedException {
 		Scanner teclado = new Scanner(System.in);
-		double cura;
-		double dmg;
-		boolean escudo;
-		double hp1 = 0;
-		double hp2 = 0;
 		int mmenu;
 		int mmenuesp;
 		int mmenumagia;
-		double mp1 = 0;
-		double mp2 = 0;
 		boolean noataque;
 		boolean rendicion;
-		char teclamenu;
 		char teclaespecialmenu;
 		char teclamagiamenu;
-		char teclaelegirmagia1 = '1';
-		char teclaelegirmagia2 = '2';
-		char teclaelegirespecial1 = '1';
-		char teclaelegirespecial2 = '2';
-		char teclaelegirespecial3 = '3';
 		char tecla;
-		double turno;
-		double vidae = 0;
+		int turno;
 		System.out.println("Elige tu Modo de Juego:");
 		System.out.println("");
 		System.out.println("");
@@ -157,41 +141,42 @@ public class Joc {
 			}
 		}
 		turno = 0;
-		escudo = false;
+		escudoJugador = false;
+		escudoMaquina = false;
 		rendicion = false;
-		while (hp2>0 && hp1>0 && rendicion==false) {
-			turno = turno+1;
-			System.out.println(""); // no hay forma directa de borrar la consola en Java
-			System.out.println("Turno "+(int)turno);
+		while (hp2 > 0 && hp1 > 0 && rendicion == false) {
+			turno = turno + 1;
+			System.out.println("");
+			System.out.println("Turno "+ turno);
 			System.out.println("");
 			System.out.println("");
 			if (dificultad==1) {
-				System.out.println("VIDA DE JUGADOR: "+ (int)hp1 +"/4000");
-				System.out.println("MAGIA DE JUGADOR: "+(int)mp1+"/700");
+				System.out.println("VIDA DE JUGADOR: "+ hp1 +"/4000");
+				System.out.println("MAGIA DE JUGADOR: "+ mp1 +"/700");
 				System.out.println("");
-				System.out.println("VIDA DE MAQUINA: "+(int)hp2+"/3000");
-				System.out.println("MAGIA DE MAQUINA: "+(int)mp2+"/600");
+				System.out.println("VIDA DE MAQUINA: "+ hp2 +"/3000");
+				System.out.println("MAGIA DE MAQUINA: "+ mp2 +"/600");
 			}
 			else if (dificultad==2) {
-				System.out.println("VIDA DE JUGADOR: "+(int)hp1+"/3000");
-				System.out.println("MAGIA DE JUGADOR: "+(int)mp1+"/600");
+				System.out.println("VIDA DE JUGADOR: "+ hp1 +"/3000");
+				System.out.println("MAGIA DE JUGADOR: "+ mp1 +"/600");
 				System.out.println("");
-				System.out.println("VIDA DE MAQUINA: "+(int)hp2+"/3000");
-				System.out.println("MAGIA DE MAQUINA: "+(int)mp2+"/600");
+				System.out.println("VIDA DE MAQUINA: "+ hp2 +"/3000");
+				System.out.println("MAGIA DE MAQUINA: "+ mp2 +"/600");
 			}
 			else if (dificultad==3) {
-				System.out.println("VIDA DE JUGADOR: "+(int)hp1+"/3500");
-				System.out.println("MAGIA DE JUGADOR: "+(int)mp1+"/650");
+				System.out.println("VIDA DE JUGADOR: "+ hp1 +"/3500");
+				System.out.println("MAGIA DE JUGADOR: "+ mp1 +"/650");
 				System.out.println("");
-				System.out.println("VIDA DE MAQUINA: "+(int)hp2+"/4000");
-				System.out.println("MAGIA DE MAQUINA: "+(int)mp2+"/700");
+				System.out.println("VIDA DE MAQUINA: "+ hp2 +"/4000");
+				System.out.println("MAGIA DE MAQUINA: "+ mp2 +"/700");
 			}
 			else if (dificultad==4) {
-				System.out.println("VIDA DE JUGADOR: "+(int)hp1+"/3000");
-				System.out.println("MAGIA DE JUGADOR: "+(int)mp1+"/700");
+				System.out.println("VIDA DE JUGADOR: "+ hp1 +"/3000");
+				System.out.println("MAGIA DE JUGADOR: "+ mp1 +"/700");
 				System.out.println("");
-				System.out.println("VIDA DE MAQUINA: "+(int)hp2+"/5000");
-				System.out.println("MAGIA DE MAQUINA: "+(int)mp2+"/750");
+				System.out.println("VIDA DE MAQUINA: "+ hp2 +"/5000");
+				System.out.println("MAGIA DE MAQUINA: "+ mp2 +"/750");
 			}
 			System.out.println("");
 			System.out.println("");
@@ -204,33 +189,24 @@ public class Joc {
 			System.out.println("");
 			System.out.println("Rendirse -> 4");
 			System.out.println("");
-			teclamenu = '1';
-			teclamagiamenu = '2';
-			teclaespecialmenu = '3';
-			char teclarendicion = '4';
 			// TURNO DE JUGADOR
 			tecla = teclado.next().charAt(0);
 			// Ataque Normal
-			if (tecla <= '4' && tecla >= '1') {
-				if (teclamenu == tecla) {
-					dmg = ataque();
-					hp2 = hp2-dmg;
-					System.out.println("Has realizado "+(int)dmg+" puntos de damage a la vida del enemigo");
+			if (tecla <= 4 && tecla >= 1) {
+				if (tecla == 1) {
+					daño((int)(Math.random() * ((150 - 100) + 1)) + 100, 0, 2);
 					Thread.sleep(3*1000);
 				}
 				// Menu de Magia
-				else if (teclamagiamenu == tecla) {
+				else if (tecla == 2) {
 					System.out.println("Elige la Magia:");
 					System.out.println("1- BioAttack: -50 MP");
 					System.out.println("2- FireWave: -25 MP");
 					teclamagiamenu = teclado.next().charAt(0);
 					// BioAttack
-					if (teclamagiamenu == teclaelegirmagia1) {
-						if (mp1>=50) {
-							dmg = bioattack();
-							mp1 = mp1-50;
-							hp2 = hp2-dmg;
-							System.out.println("Has realizado "+(int)dmg+" puntos de damage a la vida del enemigo");
+					if (teclamagiamenu == 1) {
+						if (mp1 >= 50) {
+							daño((int)(Math.random() * ((650 - 250) + 1)) + 250, 50, 2);
 							Thread.sleep(3*1000);
 						} else {
 							System.out.println("Turno Saltado. No tienes MP suficiente");
@@ -238,12 +214,9 @@ public class Joc {
 						}
 					}
 					// FireWave
-					else if (teclamagiamenu == teclaelegirmagia2) {
+					else if (teclamagiamenu == 2) {
 						if (mp1>=25) {
-							dmg = firewave();
-							mp1 = mp1-25;
-							hp2 = hp2-dmg;
-							System.out.println("Has realizado "+(int)dmg+" puntos de damage a la vida del enemigo");
+							daño((int)(Math.random() * ((450 - 150) + 1)) + 150, 0, 2);
 							Thread.sleep(3*1000);
 						} 
 						else {
@@ -253,35 +226,30 @@ public class Joc {
 					}
 				}
 				// Menu Especial
-				else if (tecla == teclaespecialmenu) {
+				else if (tecla == 3) {
 					System.out.println("Elige tu Ataque Especial:");
-					System.out.println("1- Heal: -75 MP");
+					System.out.println("1- Heal: -75 MP (MAX 3000 HP)");
 					System.out.println("2- E.Shield: -45 MP");
 					System.out.println("3- Destruction: -150 MP");
 					teclaespecialmenu = teclado.next().charAt(0);
 					// Heal
-					if (teclaelegirespecial1 == teclaespecialmenu) {
-						if (mp1>=75 && hp1<3000) {
-							cura = heal();
-							hp1 = hp1+cura;
-							mp1 = mp1-75;
-							System.out.println("Te has recuperado "+(int)cura+" puntos de vida");
+					if (teclaespecialmenu == 1) {
+						if (mp1 >= 75 && hp1 < 3000) {
+							curacion((int)(Math.random() * ((600 - 500) + 1)) + 500, 75, 1);
 							Thread.sleep(3*1000);
-							if (hp1<3000) {
+							if (hp1 < 3000) {
 								hp1 = 3000;
+								System.out.println("No te puedes curar más de 3000 de vida.");
 							}
 						} else {
-							System.out.println("Turno Saltado. No tienes MP suficiente o tu HP esta al maximo");
+							System.out.println("Turno Saltado. No tienes MP suficiente o tu HP esta por encima de 3000");
 							Thread.sleep(3*1000);
 						}
 					}
 					// Escudo
-					else if (teclaelegirespecial2 == teclaespecialmenu) {
+					else if (teclaespecialmenu == 2) {
 						if (mp1>=45) {
-							mp1 = mp1-45;
-							escudo = true;
-							vidae = 2;
-							System.out.println("Has usado un escudo que durara 2 ataques enemigos!");
+							escudo(2, 45, 1);
 							Thread.sleep(3*1000);
 						} else {
 							System.out.println("Turno Saltado. No tienes MP suficiente");
@@ -289,27 +257,17 @@ public class Joc {
 						}
 					}
 					// Destruction
-					else if (teclaelegirespecial3 == teclaespecialmenu) {
+					else if (teclaespecialmenu == 3) {
 						if (mp1>=150) {
-							mp1 = mp1-150;
-							dmg = destruction();
-							hp2 = hp2-dmg;
-							System.out.println("Has realizado "+(int)dmg+" puntos de damage a la vida del enemigo");
+							daño((int)(Math.random() * ((900 - 100) + 1)) + 100, 150, 2);
 							Thread.sleep(2*1000);
-							if (dmg>450) {
-								System.out.println("El ataque fue efectivo!");
-								Thread.sleep(3*1000);
-							} else {
-								System.out.println("El ataque no le hizo ni cosquillas!");
-								Thread.sleep(3*1000);
-							}
 						} else {
 							System.out.println("Turno Saltado. No tienes MP suficiente");
 							Thread.sleep(3*1000);
 						}
 					}
 				}
-				else if (tecla == teclarendicion) {
+				else if (tecla == 4) {
 					rendicion = true;
 					System.out.println("TE HAS RENDIDO!!");
 					Thread.sleep(3*1000);
@@ -328,20 +286,17 @@ public class Joc {
 				mmenuesp = 0;
 			}
 			noataque = true;
-			while (noataque && escudo==false && rendicion==false && hp2>0) {
+			while (noataque && rendicion == false && hp2 > 0) {
 				mmenu = (int)(Math.random() * ((4 - 1) + 1)) + 1;//aleatorio(1,4);
 				// Ataque Normal
-				if (hp2>1500 && mp2>=75) {
+				if (hp2 > 1500 && mp2 >= 75) {
 					mmenu = 4;
 				}
-				if (mp2<25) {
+				if (mp2 < 25) {
 					mmenu = 1;
 				}
 				if (mmenu==1) {
-					dmg = ataque2();
-					hp1 = hp1-dmg;
-					System.out.println("Enemigo ataca!");
-					System.out.println("Has recibido "+(int)dmg+" puntos de damage a tu vida");
+					daño((int)(Math.random() * ((200 - 150) + 1)) + 150, 0, 1);
 					noataque = false;
 					Thread.sleep(3*1000);
 				}
@@ -349,74 +304,48 @@ public class Joc {
 				if (mmenu==2 || mmenu==3) {
 					mmenumagia = (int)(Math.random() * ((2 - 1) + 1)) + 1;//aleatorio(1,2);
 					if (mmenumagia==1 && mp2>=25) {
-						dmg = sdash();
-						mp2 = mp2-25;
-						hp1 = hp1-dmg;
 						System.out.println("Enemigo uso: SDash!");
-						System.out.println("Has recibido "+(int)dmg+" puntos de damage a tu vida");
+						daño((int)(Math.random() * ((500 - 250) + 1)) + 250, 25, 1);
 						noataque = false;
 						Thread.sleep(3*1000);
 					}
 					if (mmenumagia==2 && mp2>=80) {
-						dmg = shadoworb();
-						mp2 = mp2-80;
-						hp1 = hp1-dmg;
+						
 						System.out.println("Enemigo uso: Shadow Orb!");
-						System.out.println("Has recibido "+(int)dmg+" puntos de damage a tu vida");
+						daño((int)(Math.random() * ((400) + 1)) + 400, 80, 1);
 						noataque = false;
 						Thread.sleep(3*1000);
 					}
 				}
 				// Menu ESP
-				if (mmenu==4) {
+				if (mmenu == 4) {
 					mmenuesp = (int)(Math.random() * ((3 - 1) + 1)) + 1;//aleatorio(1,3);
-					if (mmenuesp==1 && mp2>=75 && hp2>2000) {
-						cura = heal2();
-						mp2 = mp2-75;
-						hp2 = hp2+cura;
-						if (hp2>3000) {
+					if (mmenuesp == 1 && mp2 >= 75 && hp2 > 2000) {
+						
+						System.out.println("Enemigo uso: HEAL!");
+						curacion((int)(Math.random() * ((550) + 1)) + 550, 75, 2);
+						if (hp2 > 3000) {
 							hp2 = 3000;
 						}
-						System.out.println("Enemigo uso: HEAL!");
-						System.out.println("");
-						System.out.println("El enemigo se ha recuperado "+(int)cura+" puntos de vida");
 						noataque = false;
 						Thread.sleep(3*1000);
 					}
 					if (mmenuesp==2 && mp2>=50) {
-						dmg = arain();
-						mp2 = mp2-50;
-						hp1 = hp2-dmg;
 						System.out.println("Enemigo uso: Lluvia Acida!");
-						System.out.println("Has recibido damage Continuado.");
-						System.out.println("Has recibido un total de "+(int)dmg+" puntos de damage a tu vida");
+						daño((int)(Math.random() * ((750 - 150) + 1)) + 150, 80, 1);
 						noataque = false;
 						Thread.sleep(4*1000);
 					}
 					if (mmenuesp==3 && mp2>=120) {
-						dmg = blackhole();
-						hp1 = hp1-dmg;
-						mp2 = mp2-140;
 						System.out.println("Enemigo uso: BLACK HOLE!");
-						System.out.println("Es muy efectivo contra ti!");
-						System.out.println("Has recibido un total de "+(int)dmg+" puntos de damage a tu vida");
+						daño((int)(Math.random() * ((1400 - 500) + 1)) + 500, 140, 1);
 						noataque = false;
 						Thread.sleep(4*1000);
 					}
 				}
 			}
-			if (escudo==true) {
-				System.out.println("Has bloqueado el ataque enemigo!");
-				Thread.sleep(3*1000);
-				vidae = vidae-1;
-				if (vidae==0) {
-					escudo = false;
-					System.out.println("El ESCUDO se ha roto!");
-					Thread.sleep(3*1000);
-				}
-			}
 		}
-		System.out.println(""); // no hay forma directa de borrar la consola en Java
+		System.out.println("");
 		if (hp2<=0) {
 			System.out.println("Has Ganado!!");
 			System.out.println("");
@@ -429,8 +358,8 @@ public class Joc {
 			System.out.println("");
 			Thread.sleep(2*1000);
 		}
-		System.out.println("VIDA DE JUGADOR: "+(int)hp1);
-		System.out.println("VIDA DE MAQUINA: "+(int)hp2);
+		System.out.println("VIDA DE JUGADOR: "+ hp1);
+		System.out.println("VIDA DE MAQUINA: "+ hp2);
 		}
 
 	public static void main(String args[]) throws IOException, InterruptedException {
